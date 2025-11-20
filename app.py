f037326fb37fabe1485979e9013c5bcc92506b13
Avi Bauer <bauerac@bc.edu>
2025-11-20 16:08:50 -0500

    Wrapped return attributes in quotes

--- app.py
+++ app.py
@@ -133,12 +133,12 @@ async def predict(input: TextInput):
     df = pd.DataFrame(prediction, columns=col_names)
     cols = df.columns[(df == 1).any()].tolist()
     prediction_string = ", ".join(cols)
-    return_body = jsonable_encoder(ModelOutput(prediction = prediction_string))
+    return_body = jsonable_encoder(ModelOutput('prediction' = prediction_string))
   
     # reutrn data
     return JSONResponse(content=return_body)
     
   except Exception as e:
     # Return error
-    return {error: e}
+    return {'error': e}
     
